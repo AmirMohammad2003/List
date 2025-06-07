98fe5b7d7744fa021686eb7f58f851522eb66989
AmirMohammadG <AmirMohammadG@WSN-MIS-158>
2025-06-07 12:33:36 +0330

    Add project files.

---
+++ FakeList.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class FakeList<T> : IEquatable<FakeList<T>>, IEnumerable<T>, ICollection<T>, IList<T>
+        where T : IEquatable<T>
+    {
+        T[] items;
+        int cap;
+        int size;
+        public int Count
+        {
+            get { return size; }
+        }
+
+        public bool IsReadOnly => false;
+
+        public T this[int index] { get => items[index]; set => throw new NotImplementedException(); }
+
+        public FakeList(int capacity = 10)
+        {
+            cap = capacity;
+            items = new T[cap];
+            size = 0;
+        }
+
+        private void Resize()
+        {
+            cap *= 2;
+            T[] newItems = new T[cap];
+            for (int i = 0; i < size; i++)
+            {
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
+
+        public void Add(T item)
+        {
+            if (size == cap)
+            {
+                Resize();
+            }
+            items[size++] = item;
+        }
+
+        public void RemoveAt(int b)
+        {
+            --size;
+            for (int i = b; i < size; i++)
+            {
+                items[i] = items[i + 1];
+            }
+        }
+
+        public void Clear()
+        {
+            size = 0;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (size == cap)
+            {
+                Resize();
+            }
+            for (int i = size - 1; i >= index; i--)
+            {
+                items[i + 1] = items[i];
+            }
+            items[index] = item;
+            ++size;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1; 
+        }
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index == -1) return false;
+            RemoveAt(index);
+            return true;
+        }
+
+
+        public override bool Equals(object? obj) => this.Equals(obj as FakeList<T>);
+
+        public bool Equals(FakeList<T>? other)
+        {
+            if (other is null) return false;
+
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            if (this.GetType() != other.GetType()) return false;
+
+            if (this.Count != other.Count) return false;
+
+            if (this.Count == 0) return true;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (!this.items[i].Equals(other.items[i])) return false;
+            }
+            return true;
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < size; i++) yield return items[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0 || arrayIndex >= array.Length) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (array.Length < size - arrayIndex) throw new ArgumentException("Not enough space in the target array.");
+            for (int i = 0; i < size; i++)
+            {
+                array[arrayIndex + i] = items[i];
+            }
+        }
+
+
+        public override int GetHashCode()
+        {
+            int prime = 31;
+            int hash = 17;
+            for (int i = 0; i < size; i++)
+            {
+                hash = hash * prime + (items[i] == null ? 0 : items[i].GetHashCode());
+            }
+            return hash;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FakeList<int> list = new FakeList<int>();
+            FakeList<int> list2 = new FakeList<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(2);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(8);
+            list.Add(7);
+            list.RemoveAt(6);
+            list.Remove(2);
+            list.Insert(4, 47);
+            list2.Add(1);
+            list2.Add(3);
+            list2.Add(2);
+            list2.Add(4);
+            list2.Add(5);
+            list2.Add(6);
+            list2.Add(8);
+            list2.Add(7);
+            list2.RemoveAt(6);
+            list2.Remove(2);
+            list2.Insert(4, 47);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            foreach (var item in list2)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(list.Equals(list2));
+            Console.WriteLine(list.Contains(47));
+            list.Clear();
+            Console.WriteLine(list.Count);
+        }
+    }
+}
